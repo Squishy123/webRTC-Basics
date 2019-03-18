c29ea2378c7f22b7d54c456c7be06ed1e96306f8
Christian Wang <christian.gnaw@gmail.com>
2019-03-18 18:12:05 -0400

    Added basic restify web hosting

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const restify = require('restify');
+
+let server = restify.createServer();
+
+server.get('/*', restify.plugins.serveStatic({
+    directory: path.join(__dirname, '/static')
+}));
+
+server.listen(8000, () => {
+        console.log(`Server listening on ${server.name}, ${server.url}`);
+});
