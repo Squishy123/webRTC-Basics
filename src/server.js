049d79c0d353fe4b40fabc00ea5cb757024ba4f9
Christian Wang <christian.gnaw@gmail.com>
2019-03-18 21:01:25 -0400

    Added working login username and logout events and handling

--- src/server.js
+++ src/server.js
@@ -1,17 +1,71 @@
 const path = require('path');
-
 const restify = require('restify');
+const WebSocket = require('ws');
 
+//web server
 let server = restify.createServer();
 
+//wildcard static serving
 server.get('/*', restify.plugins.serveStatic({
     directory: path.join(__dirname, '/static')
 }));
 
+//serve index.html to root 
 server.get('/', restify.plugins.serveStatic({
     directory: path.join(__dirname, '/static/index.html')
 }))
 
 server.listen(8000, () => {
         console.log(`Server listening on ${server.name}, ${server.url}`);
+});
+
+//ws server
+let wsServer = new WebSocket.Server({port: 3000});
+//keep track of users
+let users = [];
+
+//wrapper function for sending
+function sendTo(ws, data) {
+    ws.send(JSON.stringify(data));
+}
+
+wsServer.on('connection', ws => {
+    console.log('Client Connected!');
+
+
+    ws.on('message', message => {
+        let data = null;
+
+        try {
+            data = JSON.parse(message);
+        } catch(err) {
+            console.error(err);
+        }
+
+        switch(data.type) {
+            case 'login':
+                //check if username is unique
+                if(users[data.username]) {
+                    sendTo(ws, {type: 'login', success: false});
+                } else {
+                    users[data.username] = ws;
+                    ws.username = data.username;
+                    sendTo(ws, {type: 'login', success: true});
+                    console.log(`User ${data.username} logged in!`);
+                }
+                break;
+            case 'logout':
+                users[data.username] = null;
+                sendTo(ws, {type: 'logout', success: true});
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        let k = [...users.keys()].find((e) => {
+            return users[e] == ws;
+        });
+        console.log(`User ${users[k].username} logged off!`);
+        users[k] = null;
+    })
 });
--- src/static/main.js
+++ src/static/main.js
@@ -1,5 +1,10 @@
 //store
-let stream, videoElement = document.querySelector('#vidStream'), accessButton = document.querySelector('#getAccess');
+let stream, videoElement = document.querySelector('#vidStream')
+    , accessButton = document.querySelector('#getAccess')
+    , loginElement = document.querySelector('.login')
+    , usernameElement = document.querySelector('#username')
+    , loginButton = document.querySelector('#login')
+    , alertsElement = document.querySelector('.alerts');
 
 //give webcam access and render it in video element
 async function getWebCamAccess(e) {
@@ -11,12 +16,12 @@ async function getWebCamAccess(e) {
         //set video src to stream
         videoElement.srcObject = stream;
         //button label
-        accessButton.textContent = `Close webcam`;
-   
+        accessButton.textContent = `Close Webcam`;
+
         //add close event listener
         accessButton.removeEventListener('click', getWebCamAccess);
         accessButton.addEventListener('click', removeWebCamAccess);
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 }
@@ -28,9 +33,9 @@ async function removeWebCamAccess(e) {
             track.stop();
         });
         //set src = null
-        videoElement.srcObject  = null;
+        videoElement.srcObject = null;
         //button label
-        accessButton.textContent = `Gain Access To Camera`;
+        accessButton.textContent = `Open Webcam`;
 
         //add open event listener
         accessButton.removeEventListener('click', removeWebCamAccess);
@@ -42,3 +47,110 @@ async function removeWebCamAccess(e) {
 
 //add button listener
 accessButton.addEventListener('click', getWebCamAccess);
+
+
+//setup websocket connection
+let ws = new WebSocket('ws://localhost:3000');
+
+ws.onopen = () => {
+    console.log(`Connected to WS Server!`);
+}
+
+ws.onerror = err => {
+    console.error(err);
+}
+
+ws.onmessage = msg => {
+    console.log(`Message received: ${msg}`);
+
+    let data = JSON.parse(msg.data);
+    switch (data.type) {
+        case 'login':
+            handleLogin(data.success);
+            break;
+        case 'logout':
+            handleLogout(data.success);
+            break;
+    }
+}
+
+//wrapper for sending message via ws
+function sendMessage(msg) {
+    ws.send(JSON.stringify(msg));
+}
+
+//creates an alert that removes itself after 5 seconds
+function addAlert(msg) {
+    //if there are too many alerts remove the oldest one
+    if (alertsElement.children.length > 2) {
+        alertsElement.removeChild(alertsElement.firstElementChild);
+    }
+
+    let alert = document.createElement('div');
+    alert.className = 'alert';
+    alert.textContent = msg;
+    alertsElement.appendChild(alert);
+
+
+    setTimeout(function () {
+        alert.remove();
+    }, 1000)
+}
+
+
+function loginReq(e) {
+    //check that username is legit
+    if (usernameElement.value.length <= 0) {
+        addAlert('ERROR: Please enter a username');
+        return;
+    }
+
+    addAlert(`Logging in with username ${usernameElement.value}`);
+    //send login req to server
+    sendMessage({
+        type: 'login',
+        username: usernameElement.value
+    });
+}
+
+function handleLogin(success) {
+    if (!success)
+        addAlert(`ERROR: Login unsuccessful, username already taken`)
+    else {
+        addAlert(`Login Successful!`);
+        [...loginElement.children].forEach(c => {
+            if(c != loginButton)
+                c.classList.add('hidden');
+        });
+        loginButton.textContent = "logout";
+        loginButton.removeEventListener('click', loginReq);
+        loginButton.addEventListener('click', logoutReq);
+    }
+}
+
+//add listener for initial login
+loginButton.addEventListener('click', loginReq);
+
+function logoutReq(e) {
+    addAlert(`Logging out with username ${usernameElement.value}`);
+
+    sendMessage({
+        type: 'logout',
+        username: usernameElement.value
+    });
+}
+
+function handleLogout(success) {
+    if(!success) {
+        addAlert(`ERROR: Logout Unsuccessful!`);
+    } else {
+        addAlert('Logout Successful!');
+        [...loginElement.children].forEach(c => {
+            if(c != loginButton)
+                c.classList.remove('hidden');
+        });
+        loginButton.textContent = "login";
+        loginButton.removeEventListener('click', logoutReq);
+        loginButton.addEventListener('click', loginReq);
+    }
+}
